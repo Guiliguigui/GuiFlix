ec3a371b39288907e8abb40dfdf7a03d317c6576
Guiliguigui <gui.mairesse@gmail.com>
2022-04-04 15:39:36 +0200

    AdminRepository

---
+++ GuiFlix/GuiFlix_Repositories/Repositories/AdminRepository.cs
@@ -0,0 +1,17 @@
+﻿using GuiFlix_Models.Models;
+using GuiFlix_Repositories.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuiFlix_Repositories.Repositories
+{
+    internal class AdminRepository : GenericRepository<Admin>
+    {
+        public AdminRepository(ApplicationDbContext applicationDbContext) : base(applicationDbContext)
+        {
+        }
+    }
+}
