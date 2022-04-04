c6a7b95ce62a00bf2396fd750b107071cf4eb0a9
Guiliguigui <gui.mairesse@gmail.com>
2022-04-04 09:46:53 +0200

    IRepository et BaseRepository

---
+++ GuiFlix/GuiFlix_Repositories/Repositories/BaseRepository.cs
@@ -0,0 +1,13 @@
+﻿using GuiFlix_Repositories.Data;
+
+namespace GuiFlix_Repositories.Repositories
+{
+    internal class BaseRepository
+    {
+        protected readonly ApplicationDbContext _db;
+        public BaseRepository(ApplicationDbContext applicationDbContext)
+        {
+            _db = applicationDbContext;
+        }
+    }
+}
---
+++ GuiFlix/GuiFlix_Repositories/Repositories/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace GuiFlix_Repositories.Repositories
+{
+    internal interface IRepository<T> where T : class
+    {
+        Task<T> Create(T entity);
+        Task<T> Find(int id);
+        Task<T> Find(Expression<Func<T, bool>> predicate);
+        Task<IEnumerable<T>> FindAll();
+        Task<IEnumerable<T>> FindAll(Expression<Func<T, bool>> predicate);
+        Task<T> Update(T entity);
+    }
+}
