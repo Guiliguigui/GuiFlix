5d9fe042489a5650034a7a1626655e10233988d4
Guiliguigui <gui.mairesse@gmail.com>
2022-04-04 15:29:34 +0200

    AccountRepository

---
+++ GuiFlix/GuiFlix_Repositories/Repositories/AccountRepository.cs
@@ -0,0 +1,30 @@
+﻿using GuiFlix_Models.Models;
+using GuiFlix_Repositories.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuiFlix_Repositories.Repositories
+{
+    internal class AccountRepository : GenericRepository<Account>
+    {
+        public AccountRepository(ApplicationDbContext applicationDbContext) : base(applicationDbContext)
+        {
+        }
+
+        public override async Task<Account> Find(int id)
+        {
+            return await _db.Accounts.Include(e => e.Profiles)
+                                     .FirstOrDefaultAsync(e => e.Id == id);
+        }
+        public override async Task<Account> Find(Expression<Func<Account, bool>> predicate)
+        {
+            return await _db.Accounts.Include(e => e.Profiles)
+                                     .FirstOrDefaultAsync(predicate);
+        }
+    }
+}
