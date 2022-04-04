4f7d3ca1c636af7cf30ba08f3e4607726a2069ce
Guiliguigui <gui.mairesse@gmail.com>
2022-04-04 17:13:42 +0200

    MediaRepository

---
+++ GuiFlix/GuiFlix_Repositories/Repositories/MediaRepository.cs
@@ -0,0 +1,49 @@
+﻿using GuiFlix_Models.Models;
+using GuiFlix_Repositories.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace GuiFlix_Repositories.Repositories
+{
+    public class MediaRepository : GenericRepository<Media>
+    {
+        public MediaRepository(ApplicationDbContext applicationDbContext) : base(applicationDbContext)
+        {
+        }
+
+        public override async Task<Media> Find(int id)
+        {
+            var media = await _db.Medias.FirstOrDefaultAsync(e => e.Id == id);
+            if(media.Type == nameof(Film))
+                return await _db.Films.Include(e => e.Categories)
+                    .Include(e => e.CrewMembers)
+                    .ThenInclude(e => e.CrewMember)
+                    .FirstOrDefaultAsync(e => e.Id == id);
+            else
+                return await _db.TVShows.Include(e => e.Categories)
+                    .Include(e => e.CrewMembers)
+                    .ThenInclude(e => e.CrewMember)
+                    .Include(e => e.Episodes)
+                    .FirstOrDefaultAsync(e => e.Id == id);
+        }
+
+        public override async Task<Media> Find(Expression<Func<Media, bool>> predicate)
+        {
+
+            var media = await _db.Medias.FirstOrDefaultAsync(predicate);
+            if (media.Type == nameof(Film))
+                return await _db.Films.Include(e => e.Categories)
+                    .Include(e => e.CrewMembers)
+                    .ThenInclude(e => e.CrewMember)
+                    .FirstOrDefaultAsync(predicate);
+            else
+                return await _db.TVShows.Include(e => e.Categories)
+                    .Include(e => e.CrewMembers)
+                    .ThenInclude(e => e.CrewMember)
+                    .Include(e => e.Episodes)
+                    .FirstOrDefaultAsync(predicate);
+        }
+    }
+}
