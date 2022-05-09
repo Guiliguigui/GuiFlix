395279607b0df8b62acf2510b5483ce2c174ae4e
YOANN LEMAIRE <yoann.lemaire@labom2iformation.fr>
2022-05-09 14:55:40 +0200

    Create BrowserRouter

--- guiflix_reactjs/src/App.js
+++ guiflix_reactjs/src/App.js
@@ -1,8 +1,28 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import LoginView from './Views/LoginView/LoginView';
+import ProfileView from './Views/ProfilesView/ProfileView';
+import FilmView from './Views/FilmView/FilmView';
+import TvShowView from './Views/TvShowView/TvShowView';
+import NavigationView from './Views/NavigationView/NavigationView';
+import RegistrerView from './Views/RegistrerView/RegistrerView';
+import HomeView from './Views/HomeView/HomeView';
+
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<LoginView />} />
+          <Route path="/" element={<HomeView />} />
+          <Route path="/navigation" element={<NavigationView />} />
+          <Route path="/profile" element={<ProfileView />} />
+          <Route path="/film" element={<FilmView />} />
+          <Route path="/tvshow" element={<TvShowView />} />
+          <Route path="/registrer" element={<RegistrerView />} />
+        </Routes>
+      </BrowserRouter>
       
     </div>
   );
