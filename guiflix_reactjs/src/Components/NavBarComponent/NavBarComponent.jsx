0a8a2fd4ed3d2d4758ed6f7517184faa43493f2a
Romaric <kiipoh98@gmail.com>
2022-05-13 12:03:27 +0200

    update responsive components

--- guiflix_reactjs/src/Components/NavBarComponent/NavBarComponent.jsx
+++ guiflix_reactjs/src/Components/NavBarComponent/NavBarComponent.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
+import Logo from '../../Assets/LogoGuiFlix.png';
+import Icon from '../../Assets/GuiFlixIcon.png';
 import { Link } from 'react-router-dom';
 import './navbarcomponent.css';
 
 export default function NavBarComponent() {
     return (<div className='NavBar'>
-        <Link to="/movies">Movies</Link>
-        <Link to="/shows">Shows</Link>
-        <Link to="/news">News</Link>
+        <div className="largeScreen">
+        <Link to="/"  ><img src={Logo} className="p-2" alt="LogoGuiFlix"  width={150} /></Link>
+        <Link className="linkNavBar" to="/movies">Movies</Link>
+        <Link className="linkNavBar" to="/shows">Shows</Link>
+        <Link className="linkNavBar" to="/news">News</Link>
         {//Component profiles
         }
+        </div>
+        <div className="smallScreen">
+        <Link to="/"  ><img src={Icon} className="p-2" alt="LogoGuiFlix"  width={40} /></Link>
+        <Link className="linkNavBar" to="/movies" title='Movies'>M</Link>
+        <Link className="linkNavBar" to="/shows" title='Shows'>S</Link>
+        <Link className="linkNavBar" to="/news" title='News'>N</Link>
+        </div>
     </div>
     );
 }
--- guiflix_reactjs/src/Components/QuestionComponent/QuestionComponent.jsx
+++ guiflix_reactjs/src/Components/QuestionComponent/QuestionComponent.jsx
@@ -4,17 +4,25 @@ import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default function QuestionComponent(props) {
-    const Display = ()=>{
-        document.getElementById(props.Question.id).style.display === "none" ? document.getElementById(props.Question.id).style.display = "" : document.getElementById(props.Question.id).style.display = "none";
+    const Display = () => {
+        if (document.getElementById(props.Question.id).style.display === "none") {
+            document.getElementById(props.Question.id).style.display = "";
+            document.getElementById("icon" + props.Question.id).style.transform = "rotate(45deg)";
+        }
+        else {
+            document.getElementById(props.Question.id).style.display = "none";
+            document.getElementById("icon" + props.Question.id).style.transform = "rotate(0deg)";
+        }
     }
     return (
         <div className="questionAccordion">
             <div className="headerAccordion" >
                 <button className="buttonAccordion" onClick={Display}>
-                    {props.Question.title}<FontAwesomeIcon icon={faPlus} />
+                    {props.Question.title}<FontAwesomeIcon id={"icon" + props.Question.id} icon={faPlus} />
+
                 </button>
             </div>
-            <div id={props.Question.id} style={{display:'none'}}>
+            <div id={props.Question.id} style={{ display: 'none' }}>
                 <div className="bodyAccordion">
                     {props.Question.answer}
                 </div>
