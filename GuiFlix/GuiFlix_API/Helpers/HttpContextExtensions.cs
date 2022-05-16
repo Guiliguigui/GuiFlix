def67fd756df9ce683ef167194e33a5c0b8ac2c0
Guiliguigui <gui.mairesse@gmail.com>
2022-05-16 14:43:15 +0200

    Added endpoint to get account with jwt

--- GuiFlix/GuiFlix_API/Helpers/HttpContextExtensions.cs
+++ GuiFlix/GuiFlix_API/Helpers/HttpContextExtensions.cs
@@ -6,13 +6,17 @@ namespace GuiFlix_API.Helpers
 {
     public static class HttpContextExtensions
     {
-        public static bool CheckRole(this HttpContext httpContext, string role)
+        //public static bool CheckRole(this HttpContext httpContext, string role)
+        //{
+        //    return httpContext.User.Claims.Where(c => c.Type == ClaimTypes.Role && c.Value == role).Count() > 0;
+        //}
+        //public static bool CheckId(this HttpContext httpContext, int id)
+        //{
+        //    return httpContext.User.Claims.Where(c => c.Type == "Id").FirstOrDefault()?.Value == id.ToString();
+        //}
+        public static int GetId(this HttpContext httpContext)
         {
-            return httpContext.User.Claims.Where(c => c.Type == ClaimTypes.Role && c.Value == role).Count() > 0;
-        }
-        public static bool CheckId(this HttpContext httpContext, int id)
-        {
-            return httpContext.User.Claims.Where(c => c.Type == ClaimTypes.NameIdentifier).FirstOrDefault()?.Value == id.ToString();
+            return int.Parse(httpContext.User.Claims.Where(c => c.Type == "Id").FirstOrDefault()?.Value);
         }
     }
 }
