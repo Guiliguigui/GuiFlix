b6fb41e6c20cbd1fef854491ff73134a000dc51f
Romaric <kiipoh98@gmail.com>
2022-05-16 14:23:36 +0200

    update HomeView

--- guiflix_reactjs/src/Views/HomeView/HomeView.jsx
+++ guiflix_reactjs/src/Views/HomeView/HomeView.jsx
@@ -7,7 +7,7 @@ export default function HomeView() {
     return (<div className="HomeView">
         <div className="divLink1">
             <Link to="/"><img src={Logo} alt="LogoGuiFlix" width={200} /></Link>
-            <Link to="/register"><button className="btn btn-danger">Sign in</button></Link>
+            <Link to="/register"><button className="btn btn-danger">Register</button></Link>
         </div>
         <div className="divText">
             <div className="h1Text">Unlimited movies, TV, shows and more.</div>
@@ -15,8 +15,9 @@ export default function HomeView() {
             <div className="fs-4">Ready to watch? Enter your email to create or restart your membership.</div>
         </div>
         <div className="divLink2">
-            <Link to="/login"><button className="btn btn-lg btn-danger me-5">Connection</button></Link>
-            <Link to="/register"><button className="btn btn-lg btn-danger">Sign in</button></Link>
+            <Link to="/login"><button className="btn btn-lg btn-danger me-5">Login</button></Link>
+            <Link to="/register"><button className="btn btn-lg btn-danger">Register</button></Link>
         </div>
-    </div>);
+    </div>
+    );
 }
