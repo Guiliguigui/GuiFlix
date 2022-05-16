b17501b41f94d7ef132589f20f591a6938f28a45
Guiliguigui <gui.mairesse@gmail.com>
2022-05-16 09:26:02 +0200

    Added Account in LoginResponseUserDTO

--- GuiFlix/GuiFlix_API/DTOs/Authentication/LoginResponseUserDTO.cs
+++ GuiFlix/GuiFlix_API/DTOs/Authentication/LoginResponseUserDTO.cs
@@ -1,4 +1,5 @@
 ﻿using GuiFlix_API.DTOs;
+using GuiFlix_Models.Models;
 
 namespace GuiFlix_API.DTOs.Authentication
 {
@@ -7,5 +8,6 @@ namespace GuiFlix_API.DTOs.Authentication
         public bool IsAuthSuccessful { get; set; }
         public string? ErrorMessage { get; set; }
         public string? Token { get; set; }
+        public Account? Account { get; set; }
     }
 }
