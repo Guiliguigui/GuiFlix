c01e4a4c6bcd1b795c54a6a9d020ffc1652edfab
Guiliguigui <gui.mairesse@gmail.com>
2022-05-17 13:39:34 +0200

    Modified NavBar

--- guiflix_reactjs/src/Components/NavBarComponent/NavBarComponent.jsx
+++ guiflix_reactjs/src/Components/NavBarComponent/NavBarComponent.jsx
@@ -1,26 +1,33 @@
 import React from 'react';
 import Logo from '../../Assets/LogoGuiFlix.png';
 import Icon from '../../Assets/GuiFlixIcon.png';
-import { Link } from 'react-router-dom';
-import { faM, faS,faN } from "@fortawesome/free-solid-svg-icons";
+import { Link, useNavigate } from 'react-router-dom';
+import { faClapperboard, faTv, faUserFriends, faSignOut} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './navbarcomponent.css';
 
 export default function NavBarComponent() {
+    
+    const navigate = useNavigate();
+
+    function logout() {
+        localStorage.removeItem('GuiFlix_JWT')
+        navigate("/")
+    }
     return (<div className='NavBar'>
         <div className="largeScreen">
-            <Link to="/"  ><img src={Logo} className="p-2" alt="LogoGuiFlix" width={150} /></Link>
-            <Link className="linkNavBar" to="/movies">Movies</Link>
-            <Link className="linkNavBar" to="/shows">Shows</Link>
-            <Link className="linkNavBar" to="/news">News</Link>
-            {//Component profiles
-            }
+            <Link to="/navigation"  ><img src={Logo} className="p-2" alt="LogoGuiFlix" width={150} /></Link>
+            <Link className="linkNavBar" to="/film"><FontAwesomeIcon icon={faClapperboard}/>&nbsp;Films</Link>
+            <Link className="linkNavBar" to="/tvshow"><FontAwesomeIcon icon={faTv}/>&nbsp;TV Shows</Link>
+            <Link className="linkNavBar navProfiles" to="/profile"><FontAwesomeIcon icon={faUserFriends}/>&nbsp;Profiles</Link>
+            <a className="linkNavBar navDisconnect" onClick={e=>logout()} ><FontAwesomeIcon icon={faSignOut}/>&nbsp;Logout</a>
         </div>
         <div className="smallScreen">
-            <Link to="/"  ><img src={Icon} className="p-2" alt="LogoGuiFlix" width={40} /></Link>
-            <Link className="linkNavBar" to="/movies" title='Movies'><FontAwesomeIcon icon={faM}/></Link>
-            <Link className="linkNavBar" to="/shows" title='Shows'><FontAwesomeIcon icon={faS}/></Link>
-            <Link className="linkNavBar" to="/news" title='News'><FontAwesomeIcon icon={faN}/></Link>
+            <Link to="/navigation"  ><img src={Icon} className="p-2" alt="LogoGuiFlix" width={40} /></Link>
+            <Link className="linkNavBar" to="/film" title='Films'><FontAwesomeIcon icon={faClapperboard}/></Link>
+            <Link className="linkNavBar" to="/tvshow" title='TV Shows'><FontAwesomeIcon icon={faTv}/></Link>
+            <Link className="linkNavBar navProfiles" to="/profile" title='Profiles'><FontAwesomeIcon icon={faUserFriends}/></Link>
+            <div className="linkNavBar navDisconnect" onClick={e=>logout()} title='Logout'><FontAwesomeIcon icon={faSignOut}/></div>
         </div>
     </div>
     );
