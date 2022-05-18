3f4d5fda5f2c849012224c673fde3feb99eb93a7
Guiliguigui <gui.mairesse@gmail.com>
2022-05-18 15:22:15 +0200

    HttpGet Like on LikeComponent

--- guiflix_reactjs/src/Components/LikesComponent/LikeComponent.jsx
+++ guiflix_reactjs/src/Components/LikesComponent/LikeComponent.jsx
@@ -1,5 +1,6 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import { get, post } from "../../Service/Service";
 import { faHeart, faThumbsUp, faThumbsDown, faHeartCirclePlus } from "@fortawesome/free-solid-svg-icons";
 import "./likecomponent.css";
@@ -7,29 +8,31 @@ import "./likecomponent.css";
 export default function LikeComponent(props) {
     const LikeTypes = [faHeart, faThumbsUp, faThumbsDown]
     const [like, setLike] = useState(faHeartCirclePlus);
-    
+    const { id } = useParams();
+    const JWT = localStorage.getItem("GuiFlix_JWT");
+
     useEffect(() => {
-        props.profileSelected.likes.map((like) => {
-            if(like.mediaId===props.mediaId)
-                setLike(LikeTypes[like.type])
+        get(`Like/IsMediaLiked?profileId=${props.profileSelected.id}&mediaId=${props.mediaId}`, {
+            headers: { Authorization: `Bearer ${JWT}` }
+        }).then((res) => {
+            setLike(LikeTypes[res.data.type])
+        }).catch((err) => {
+            setLike(faHeartCirclePlus)
         })
     }, [])
-    
-    // const JWT = localStorage.getItem("GuiFlix_JWT");
+
     // function changeLike(type) {
     //     post("LikeMedia",
     //         {
-    //             type,
-    //             props.profileSelected.profileId,
-    //             props.profileSelected.mediaId
+    //             type: type,
+    //             profileId: props.profileSelected.profileId,
+    //             mediaId: props.mediaId
     //         },
     //         {
     //             headers: { Authorization: `Bearer ${JWT}` }
     //         }
     //     ).then(res => {
-    //         if (props.mediaId !== res.data.mediaId) {
-    //             localStorage.setItem(LikeComponent);
-    //         }
+    //         setLike(LikeTypes[res.data.type])
     //     }).catch((err) => {
     //         console.error(err.message);
     //     })
