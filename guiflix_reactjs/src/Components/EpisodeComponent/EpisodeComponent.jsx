4b1797756fa824e7b2382f35c37ed2dcdaf8c6e5
Romaric <kiipoh98@gmail.com>
2022-05-18 16:08:02 +0200

    Update Player Episode component

--- guiflix_reactjs/src/Components/EpisodeComponent/EpisodeComponent.jsx
+++ guiflix_reactjs/src/Components/EpisodeComponent/EpisodeComponent.jsx
@@ -4,16 +4,18 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './episodecomponent.css';
 
 export default function EpisodeComponent(props) {
+    const playEpisode = ()=>{
+        props.setSourceLink(props.episode.sourceLink);
+        props.setCurrentEpisode(props.episode.name);
+    }
     return (
         <div className="episode">
-
             <div className="titleEpisode">
                 {props.episode.episodeNumber}&nbsp;{props.episode.name}
                 <img className="imgEpisode" src={props.episode.thumbnailLink} alt="episode" />
             </div>
             <div className="descriptionEpisode">{props.episode.description}</div>
-
-            <button className="buttonEpisode" onClick={() => { props.setSourceLink("hello") }}><FontAwesomeIcon icon={faPlayCircle} /></button>
+            <button className="buttonEpisode" onClick={() => { playEpisode() }}><FontAwesomeIcon icon={faPlayCircle} /></button>
         </div>
     )
 }
--- guiflix_reactjs/src/Views/VideoPlayerView/VideoPlayerView.jsx
+++ guiflix_reactjs/src/Views/VideoPlayerView/VideoPlayerView.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useRef, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { groupBy } from "underscore";
 import EpisodeComponent from '../../Components/EpisodeComponent/EpisodeComponent';
 import LoaderComponent from '../../Components/LoaderComponent/LoaderComponent';
 
@@ -10,21 +11,37 @@ export default function VideoPlayerView() {
     const location = useLocation();
     const navigate = useNavigate();
     const videoRef = useRef();
-    const [sourceLink,setSourceLink] = useState("");
-    const [season,setSeason] = useState("");
+    const [sourceLink, setSourceLink] = useState("");
+    const [season, setSeason] = useState([]);
     const [isLoading, setIsLoading] = useState(true)
+    const [key, setKey] = useState("")
+    const [currentEpisode,setCurrentEpisode] = useState("");
     useEffect(() => {
         setIsLoading(false);
-        location.state.type === "TVShow"? setSourceLink(location.state.episodes[0].sourceLink) : setSourceLink("");
+        if(location.state.type ==="TVShow")
+        {
+            setSourceLink(location.state.episodes[0].sourceLink)
+            setKey(location.state.episodes[0].season);
+            setCurrentEpisode(location.state.episodes[0].name)
+        }
     }, [])
-    useEffect(()=>{
+    useEffect(() => {
+        setSeason(groupBy(location.state.episodes, "season"))
+        for (var key in season) {
+            var option = document.createElement("option");
+            option.text = key;
+            option.value = key;
+            var select = document.getElementById("selectSeason");
+            select.appendChild(option);
+        }
+    }, [isLoading])
+    useEffect(() => {
         videoRef.current?.load();
-    },[sourceLink])
+    }, [sourceLink])
 
     const goBack = () => {
         navigate("/navigation");
     }
-
 
     if (isLoading) {
         return (<LoaderComponent />)
@@ -34,25 +51,28 @@ export default function VideoPlayerView() {
             <div className='videoPlayer'>
                 <button className="buttonBack" onClick={goBack}><FontAwesomeIcon icon={faArrowLeft} /></button>
                 <div className='titlePlayer'>{location.state.name}</div>
-                <video controls className='player'><source src={location.state.sourceLink} /></video>
+                <video controls className='player'><source src={location.state.sourceLink}/></video>
                 <div className='descriptionPlayer'>{location.state.description}</div>
             </div>
         )
     }
     else {
-        
         return (
             <div className='videoPlayer'>
                 <button className="buttonBack" onClick={goBack}><FontAwesomeIcon icon={faArrowLeft} /></button>
                 <div className='titlePlayer'>{location.state.name}</div>
+                <div className='currentEpisode'>{currentEpisode}</div>
                 <video controls className='player' ref={videoRef}><source id="episodePlayer" src={sourceLink} type="video/webm" />Error video not find</video>
                 <div className='descriptionPlayer'>{location.state.description}</div>
                 <div className='episodesList'>
-                    {location.state.episodes.map((episode, index) => {
-                        return (
-                            <EpisodeComponent key={index} episode={episode}  setSourceLink={setSourceLink}/>
-                        )
-                    })}
+                    <select className="selectSeason" id="selectSeason" onChange={(e) => setKey(e.target.value)}>
+                    </select>
+                    {
+                        season[key].map((episode, index) => {
+                            return (
+                                <EpisodeComponent key={index} episode={episode} setSourceLink={setSourceLink} setCurrentEpisode={setCurrentEpisode} />
+                            )
+                        })}
                 </div>
             </div>
         )
