d50ffd8f3bc09008a4b1772c067111456210f527
Romaric <kiipoh98@gmail.com>
2022-05-19 10:18:25 +0200

    update responsive component

--- guiflix_reactjs/src/Views/HomeView/HomeView.jsx
+++ guiflix_reactjs/src/Views/HomeView/HomeView.jsx
@@ -11,8 +11,8 @@ export default function HomeView() {
         </div>
         <div className="divText">
             <div className="h1Text">Unlimited movies, TV, shows and more.</div>
-            <div className="fs-2">Watch anywhere. Cancel at any time.</div>
-            <div className="fs-4">Ready to watch? Enter your email to create or restart your membership.</div>
+            <div className="displayText1">Watch anywhere. Cancel at any time.</div>
+            <div className="displayText2">Ready to watch? Enter your email to create or restart your membership.</div>
         </div>
         <div className="divLink2">
             <Link to="/login"><button className="btnHome btn-lg btn-danger me-5">Login</button></Link>
